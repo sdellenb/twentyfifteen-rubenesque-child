27a3c5def967fc871c0d089fff4824a59ba1d32f
Simon Dellenbach <sdellenb@gmail.com>
2015-02-03 12:51:44 +0100

    Add the two minimally required files with basic stuff.
    To be extended.

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Antispam Bee filter for custom RegExp patterns
+ */
+add_action(
+	'init',
+	'antispam_bee_patterns'
+);
+function antispam_bee_patterns() {
+	add_filter(
+		'antispam_bee_patterns',
+		'antispam_bee_add_custom_patterns'
+	);
+}
+function antispam_bee_add_custom_patterns($patterns) {
+	// Pattern for phony author names.
+	// Fun Fact: The last one is 'Prada' in Japanese.
+	$patterns[] = array(
+		'author' => 'moncler|north face|vuitton|handbag|burberry|outlet|dress|maillot|oakley|ralph lauren|ray ban|iphone|???'
+	);
+
+	// Pattern for phony web pages.
+	$patterns[] = array(
+		'host' => '^(www\.)?fkbook\.co\.uk$|^(www\.)?nsru\.net$|^(www\.)?goo\.gl$|^(www\.)?bit\.ly$'
+	);
+
+	// Pattern for text containing strings like 'targetted visitors'.
+	$patterns[] = array(
+		'body' => 'target[t]?ed (visitors|traffic)'
+	);
+
+	return $patterns;
+}
+
+?>
