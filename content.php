be0a8015228b04f7a0608bab832a96a6c74d3297
Simon Dellenbach <sdellenb@gmail.com>
2015-04-27 11:49:30 +0200

    Adjust indentation to match original Twenty Fifteen code.

--- content.php
+++ content.php
@@ -12,58 +12,58 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
-	// Post thumbnail.
-	twentyfifteen_post_thumbnail();
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
 	?>
 
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-			if ( in_category( 'mehrsprachig' )) :
-				echo qtrans_generateLanguageSelectCode('both'); // Show the qTranslate language switcher for the specified category only.
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+				if ( in_category( 'mehrsprachig' )) :
+			    	echo qtrans_generateLanguageSelectCode('both'); // Show the qTranslate language switcher for the specified category only.
+				endif;
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			endif;
-		else :
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		endif;
 		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
-		$oldage = 10 * 24 * 60 * 60; // Ten day in seconds.
-		$limit = date_i18n('U') - $oldage; // The unix timestamp ten days ago (unit: seconds).
-		$articledate = get_the_time('U');
+			$oldage = 10 * 24 * 60 * 60; // Ten day in seconds.
+			$limit = date_i18n('U') - $oldage; // The unix timestamp ten days ago (unit: seconds).
+			$articledate = get_the_time('U');
 
-		if ( $articledate >= $limit || is_sticky() ) {
-			// This post is newer than the limit, or a sticky post, force to show the unabridged content.
-			global $more;
-			$more = 1;
-			the_content();
-		} else {
-			// This post is older than the limit, so we allow the 'Read more...' link to appear (if there is one).
-			the_content( sprintf(
-				__( 'Continue reading %s', 'twentyfifteen' ),
-				the_title( '<span class="screen-reader-text">', '</span>', false )
+			if ( $articledate >= $limit || is_sticky() ) {
+				// This post is newer than the limit, or a sticky post, force to show the unabridged content.
+				global $more;
+				$more = 1;
+				the_content();
+			} else {
+				// This post is older than the limit, so we allow the 'Read more...' link to appear (if there is one).
+				the_content( sprintf(
+					__( 'Continue reading %s', 'twentyfifteen' ),
+					the_title( '<span class="screen-reader-text">', '</span>', false )
+				) );
+			}
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
 			) );
-		}
-
-		wp_link_pages( array(
-			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
-			'after'       => '</div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) );
 		?>
 	</div><!-- .entry-content -->
 
 	<?php
-	// Author bio.
-	if ( is_single() && get_the_author_meta( 'description' ) ) :
-		get_template_part( 'author-bio' );
-	endif;
+		// Author bio.
+		if ( is_single() && get_the_author_meta( 'description' ) ) :
+			get_template_part( 'author-bio' );
+		endif;
 	?>
 
 	<footer class="entry-footer">
